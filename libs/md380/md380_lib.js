19b49b4c64dc10d49a0b5f537d22fe66f5525393
echicken <echicken@bbs.electronicchicken.com>
2017-06-10 01:36:51 -0400

    Some fairly horrendous stuff in here, but a crapid prototype of a "status bar" nonetheless. Cheesy "demo" code in comments near bottom. I am not evil.  (Nor am I proud of this.  I am sleepy.)

--- libs/md380/md380_lib.js
+++ libs/md380/md380_lib.js
@@ -1,9 +1,123 @@
-function md380() {
+//var font_cp437_8x8 = atob("AAAAAAB+gamNjamBfn7/1/Pz1/9+cPj8fvz4cAAQOHz+fDgQABxc+f/5XBwACBw9/z0cCAAYPDwYAP//58PD5///PGZCQmY8AP/Dmb29mcP/DhEREZG+wPByio+PinIAAwf+oKCgoOADB/6goKau/FpaPOfnPFpa/nx8ODgQEAAQEDg4fHz+ACRm//9mJAD6AAD6AGCQkP6AgP6AAlvlpaeawEAODg4ODg4AASlt//9tKQEwYP5gMAAYDP4MGAAQEBBUfDgQABA4fFQQEBAAPAQEBAQEABA4fBAQfDgQDBw8fDwcDABgcHh8eHBgAAAAAAAAYPpgAOAAAOAAKP4oKP4oACRU1tZUSABiZAgQJkYADFKiskwSEgAgwAA4RIIAgkQ4ABBUODg4VBAAEBB8EBAAAQYAEBAQEBAQAAYAAgQIECBAAHyGipKifAAiQv4CAgBGioqSkmYARIKSkpJsAAgYKEr+CggA5KKioqKcADxSkpKSDADAgI6QoMAAbJKSkpJsAGCSkpKUeABmAAFmABAoRIIAJCQkJCQkAIJEKBAAQICAipBgAHyCuqqqeAA+SIiISD4Agv6SkpJsADhEgoKCRACC/oKCRDgAgv6SuoLGAIL+kriAwAA4RIKKik4A/hAQEBD+AIL+ggAMAgKC/IAAgv4QKESCAoL+ggICBgD+gEAgQID+/oBAIBD+ADhEgoJEOACC/pKQkGAAeISEjIR6AoL+kpiUYgBkkpKSkkwAwICC/oKAwPwCAgIC/ADwCAQCBAjw/AICHAIC/IJEKBAoRIKAQCIeIkCAwoaKkqLChv6CgoIAgEAgEAgEAgCCgoL+ABAgQIBAIBAAAQEBAQEBAQHAIAAEKioqKh4CgP4MEhISDBwiIiIiFAAMEhISjP4CHCoqKioYABJ+koBAABklJSUlHiCC/hAgIB4AIr4CAAYBAQEhvgCA/ggUIgIAgv4CAD4gIB4gIB4+ECAgIB4AHCIiIiIcACE/GSQkGAAYJCQZPyEAIj4SICAYABIqKioqJAAgIPwiIgQAPAICAgQ+ADAIBAIECDA8AgIcAgI8IhQIFCIAOQUFBQU+ACImKjIiABAQbIKCAO4AgoJsEBAAQICAQECAAA4SIkIiEg5wiYmNjVIAXAICAl4CABwqKqqqGABEqqqqnkIAhCoqKh6CAASqqioeAgAEKqoqHgIAGCQlJScCAFyqqqqqWACcKioqKpgAHKqqKioYAIAiPoIAQICivoKAQACAoj4CAJ4oSEgongAOFNTUFA4AIj4qqqIABCoqGhwqKj5QkJD+kpIMUpKSUgwATBISEhJMAAySUhISDAAcQoKCQhwAHIJCAgIcAFgFBQUFXgCYJEJCJJgAvAICAgK8ABgkJOckJAAWepKCQgQAqGg+aKgAgf+hoOhfCQoCERH+kJBAAAQqKqqeAgAivoIADBISUpIMABwCAiJCHABekJBQUI4AXpCIRMIeAGSUlJT0FABklJSUZAAMEqICAgQAHBAQEBAQABAQEBAQHABS9BgRK1OVDVL0GBQsVL8EXgAQKEQQKEQARCgQRCgQAFUAqgBVAKoAVapVqlWqVaq7/1Xuu1X/7v8ACAgI/wAoKCj/AAgICP8A/wAICAgPCA8AKCgoPwAoKCjvAP8A/wD/ACgoKC8gPwAoKCjoCPgACAgI+Aj4ACgoKPgACAgIDwD4CAgICAgICPgICAgICAgIDwgICAj/CAgICAgICAgICAgICAgI/wgICAj/KCgoKP8A/wgI+AjoKCg/IC8oKCgoKOgI6CgoKCgoLyAvKCj/AO8oKCgoKCgoKCgoKCgo7wDvKCgoKCjoKCgoKAgICPgI+AgIKCgoLygoKCgICAgPCA8ICPgI+AgI+CgoKCg/KCgoKA8IDwgICAgI/wj/CAgoKCj/KCgoKAgICPgADwgICAj//////////w8PDw8PDw8P/////wD/////8PDw8PDw8PAcIiIUCBQiP1RUVFQoAH5AQEBAYAAgQH5AfkBAxqqSkoLGABwiIjwgIAABfggICHAIACBAQD4gQECZpeelmQA4VJKSVDgAMk6AgE4yAAxSsrKSDAA4REQoEChEOAEaJCw0ZJgAOFSSkgB+gICAgH4AVFRUVFRUACIi+iIiAAICilIiAgACIlKKAgIAf4CAYAAGAQH+ABAQ1tYQEAAkSEgkJEgAYJCQYAAYGAAICAAEBAIB/4CAgPiAgIB4AEiYqEgAPDw8PAAAAAAAAAAAAAAAfoGpjY2pgX5+/9fz89f/fnD4/H78+HAAEDh8/nw4EAAcXPn/+VwcAAgcPf89HAgAGDw8GAD//+fDw+f//zxmQkJmPAD/w5m9vZnD/w4fERG//uDwcvqPj/pyAAMH//6goODgA//+oKCm/vxaWjzn5zxaWv58fDg4EBAAEBA4OHx8/gAkZv//ZiQA+voAAPr6AGDwkP7+gP7+Alv9pb+awEAODg4ODg4AASlt//9tKQEgYP7+YCAACAz+/gwIABAQEFR8OBAAEDh8VBAQEAA8PAQEBAQEABA4fBAQfDgQDBw8fHw8HAxgcHh8fHhwYAAAAAAAYPr6YADg4ADg4AAo/v4o/v4oACR01tZcSABiZgwYMGZGAAxe8rrsXhIAIODAADh8xoIAgsZ8OAAQVHw4OHxUEBAQfHwQEAABBwYAEBAQEBAQAAYGAAYMGDBgwIAAfP6OmrL+fAACQv7+AgIARs6akvZmAETGkpL+bAAYOGjK/v4KAOTmoqK+nAA8ftKSngwAwMCOnvDgAGz+kpL+bABg8pKW/HgAZmYAAWdmABA4bMaCACQkJCQkJACCxmw4EABAwIqa8GAAfP6Curr4eAA+fsjIfj4Agv7+kpL+bAA4fMaCgsZEAIL+/oLGfDgAgv7+krqCxgCC/v6SuIDAADh8xoKKzk4A/v4QEP7+AIL+/oIADA4Cgv78gACC/v4QOO7GAIL+/oICBg4A/v5wOHD+/gD+/mAwGP7+ADh8xoLGfDgAgv7+kpDwYAB4/ISO/noAgv7+kJj+ZgBE5rKazkQAwIL+/oLAAP7+AgL+/gD4/AYG/PgA/v4MGAz+/gDC5jwYPObCAODyHh7y4ADixo6asubOAP7+goIAgMBgMBgMBgCCgv7+ABAwYMBgMBAAAQEBAQEBAQHA4CAABC4qKjweAgCC/vwSEh4MABw+IiI2FAAMHhKS/P4CABw+Kio6GAASfv6SwEAAGT0lJR8+IACC/v4QID4eACK+vgIABgcBAb++AIL+/ggcNiIAgv7+AgA+PhgcOD4eAD4+ICA+HgAcPiIiPhwAIT8fJSQ8GAAYPCQlHz8hACI+HjIgOBgAEjoqKi4kACB8/iIkADw+AgI8PgIAODwGBjw4ADw+DhwOPjwAIjYcCBw2IgA5PQUFPz4AMiYuOjImABAQfO6CggDu7gCCgu58EBAAQMCAwEDAgAAOHjJiMh4OAHD5iY3fUgBcXgICXl4CABw+Kqq6mABAxK6qqr7eQoSuKiq+ngIAhK6qKj4eAgAELurqPh4CABg8JSUnAgBA3L6qqrrYQJy+Kiq6mACcvqoqOhgAgKI+PoKAAEDAor6+wkAAgKK+PgIAnr5oSGi+ngAOHtTUHg4AIj4+qqqiAAQuKio+PioqPn7QkP7+kgBM3pKS3kwATF4SEl5MAExeUhIeDABc3oKC3l4CAFxeQgIeHgIAWV0FBV9eAICYPGZmPJiAvL4CAr68ABg8JOfnJCQAFn7+ksJmBADU9D8/9NQA//+QkPRvHwUCAxF//pDAQAQuKqq+ngIAIr6+ggAMHhJSXkwAHB4CQl5eAgBeXlBQXg4Avr6YjL6+AGT0lPT0FABk9JT0ZAAMHrKiBgQAHBwQEBAQABAQEBAcHADy9gwYM3fdifL2DBg2bt+f3t4AEDhsRBA4bEREbDgQRGw4EFUAqgBVAKoAVapVqlWqVaq7/1Xuu1X/7v//AAgICP//ACgoKP//AAgI//8A//8ACAgPDwgPDwAoKCg/PwAoKO/vAP//AP//AP//ACgoLy8gPz8AKCjo6Aj4+AAICPj4CPj4ACgoKPj4AAgICA8PAPj4CAgICAgI+PgICAgICAgPDwgICP//CAgICAgICAgICAgICAj//wgICP//KCgo//8A//8I+PgI6OgoPz8gLy8oKCjo6Ajo6CgoKC8vIC8vKP//AO/vKCgoKCgoKCgoKCjv7wDv7ygoKCjo6CgoKAgI+PgI+PgIKCgoLy8oKCgICA8PCA8PCPj4CPj4CPj4KCgoPz8oKCgPDwgPDwgICP//CP//CCgoKP//KCgoCAgI+PgADw8ICAj//////////w8PDw8PDw8P/////wD/////8PDw8PDw8PAcPiI2HDYiAD9/VFR8KAB+fkBAYGAAQH5+QH5+QADG7rqSxsYAHD4iPjwgIAABf34EBHx4ACBgQH4+YEAAmb3n572ZADh81pLWfDgAMn7OgM5+MgAMHlLyvpwAGDwkPDwkPBgZPyY8fOS8GDh81pKSAH7+gID+fgBUVFRUVFQAIiL6+iIiAAKK2nIiAgACInLaigIAf/+A4GAGBwH//gAQENbWEBAAJGxIbCRsSABg8JDwYAAYGAAICAAIDA4D//+AgPj4gPh4AJi46EgAPDw8PAAAAAAAAA==");
+//var width_cp437_8x8 = atob("BQgICAgICAUIBwgIBwgICAgIBwUICAcIBgYICAcICAgFBAUHBwcIAwQECAYDBwIHBwYHBwgHBwcHBwIDBQcFBwcHBwcHBwcHBwQHBwcHBwcHBwcHBwcHBwcHBwUIBQgIAwcHBwcHBgcHBAcHBAcHBwcHBwcHBwcHBgcGBgIGBwcHBwcHBwcHBwcHBwUIBQcHBgcHBwcHBwcHBwcHBwYICAcEBwcHBwcGBwcHCAgCBwcICAgCBQUHBwUHBAcHBwUFBQgIBQgIBQUFBQgIBQgICAgICAUFBQUICAUFCAgFBAgHBwcHBwcIBwYHBwcICAUHBwYHBwUFBwcFAwMIBgUFBQUICAgICAgFCAcICAcICAgICAcHCAgHCAcHCAgICAgIBQUGCAcICAQFBQgHBAcDCAgHBwcIBwcHBwcDBAYHBgcIBwgICAgICAcFCAgICAgICAcIBwcHBwgIBwgFCAUICAQICAcIBwcICAUHCAUIBwcICAgHBggHCAgHBwcDBwgIBwgHCAgICAcIBwcHCAYIBwcICAcHBwgIBwgHCAgHCAgIBQcIBwcHBgcHBwgIAwgICAgIAwYGCAgGCAYICAgGBgUICAUICAUGBgYICAYICAgICAgGBQUGCAgGBQgIBQQICAcHCAcICAgHCAgHCAgGBwcHBwcFBgcIBgMDCAYFBQU=");
+
+function clamp(min, max, val) {
+  return (val >= min && val <= max ? val : (val < min ? min : max));
 }
 
-md380.prototype.ReadSecurity = function(addr) {
-  var ret;
+function md380() {
+//  LCD.setFontCustom(font_cp437_8x8, 0, width_cp437_8x8, 8);
+}
 
+md380.prototype.set_backlight = function (val, freq) {
+  analogWrite(C6, clamp(0, 1, val), { freq : freq || 120 });
+}
+
+md380.prototype.StatusBar = function () {
+
+  const SB_X = 0;
+  const SB_Y = 115;
+  const SB_W = 160;
+  const SB_H = 12;
+  const SB_IC_H = SB_H - 4;
+  const SB_IC_Y = 117;
+
+  const RSSI_X = 4;
+  const RSSI_W = 26;
+
+  const SPK_X = 74;
+
+  const BATT_X = 140;
+  const BATT_W = 16;
+
+  const COLOR_LO = [ .84, .12, .22 ];
+  const COLOR_MD = [ 1, .52, 0 ];
+  const COLOR_HI = [ 0, .75, .12 ];
+
+  const COLOR_SB = [ .77, .77, .77 ]; // Status bar
+  const COLOR_BD = [ 0, 0, 0 ]; // Borders
+
+  this.update_battery = function (val) {
+    // Clear dynamic area
+    LCD.setColor.apply(LCD, COLOR_SB);
+    LCD.fillRect(BATT_X + 1, SB_IC_Y + 1, BATT_X + BATT_W - 3, SB_IC_Y + SB_IC_H - 2);
+    // Fill with appropriate width & colour
+    LCD.setColor.apply(LCD, val >= .5 ? COLOR_HI : (val >= .25 ? COLOR_MD : COLOR_LO));
+    LCD.fillRect(BATT_X + 1, SB_IC_Y + 1, BATT_X + 1 + Math.floor((BATT_W - 4) * val), SB_IC_Y + SB_IC_H - 2);
+    // Overlay level markers
+    LCD.setColor.apply(LCD, COLOR_BD);
+    for (var n = 1; n < 6; n++) {
+        var xx = BATT_X + 1 + (2 * n);
+        LCD.drawLine(xx, SB_IC_Y + 2, xx, SB_IC_Y + SB_IC_H - 2);
+    }
+  }
+
+  this.update_rssi = function (val) {
+    var ey = SB_IC_Y + SB_IC_H - 2;
+    // Clear dynamic area
+    LCD.setColor.apply(LCD, COLOR_SB);
+    LCD.fillRect(RSSI_X + 1, SB_IC_Y, RSSI_X + RSSI_W - 2, ey);
+    // I'm not crazy about this, but it looks okay.
+    var bars = Math.round((clamp(0, 1, val) * 100) / 16);
+    if (bars < 1) return;
+    LCD.setColor.apply(LCD, bars <= 2 ? COLOR_LO : (bars <= 4 ? COLOR_MD : COLOR_HI));
+    for (var n = 0; n < bars; n++) {
+      var sx = RSSI_X + (n * 4) + 1;
+      var ex = sx + 3;
+      LCD.fillRect(sx, SB_IC_Y + 5 - n, ex, ey);
+    }
+  }
+
+  this.update_volume = function (val) {
+    var sx = SPK_X + 6;
+    // Clear dynamic area
+    LCD.setColor.apply(LCD, COLOR_SB);
+    LCD.fillRect(sx, SB_IC_Y - 1, sx + 19, SB_IC_Y + SB_IC_H);
+    // Find number of bars to draw
+    var bars = Math.round((clamp(0, 1, val) * 100) / 10);
+    if (bars < 1) return;
+    LCD.setColor.apply(LCD, COLOR_BD);
+    for (var n = 0; n < bars; n++) {
+      var _sx = sx + (n * 2);
+      var _sy = SB_IC_Y + 3 - Math.floor(n / 2);
+      var _ey = SB_IC_Y + 4 + Math.floor(n / 2);
+      LCD.drawLine(_sx, _sy, _sx, _ey);
+    }
+  }
+
+  this.draw_static = function () {
+    // Status bar background
+    LCD.setColor.apply(LCD, COLOR_SB);
+    LCD.fillRect(SB_X, SB_Y, SB_X + SB_W - 1, SB_Y + SB_H - 1);
+    LCD.setColor.apply(LCD, COLOR_BD);
+    // S-Meter outline
+    LCD.drawLine(RSSI_X, SB_IC_Y, RSSI_X, SB_IC_Y + SB_IC_H - 2);
+    LCD.drawLine(RSSI_X, SB_IC_Y + SB_IC_H - 1, RSSI_X + RSSI_W - 1, SB_IC_Y + SB_IC_H - 1);
+    LCD.drawLine(RSSI_X + RSSI_W - 1, SB_IC_Y, RSSI_X + RSSI_W - 1, SB_IC_Y + SB_IC_H - 2);
+    // Speaker icon
+    LCD.fillPoly([SPK_X, SB_IC_Y + 2, SPK_X + 2, SB_IC_Y + 2, SPK_X + 4, SB_IC_Y, SPK_X + 4, SB_IC_Y + 7, SPK_X + 2, SB_IC_Y + 5, SPK_X, SB_IC_Y + 5]);
+    // Battery outline
+    LCD.drawRect(BATT_X, SB_IC_Y, BATT_X + BATT_W - 2, SB_IC_Y + SB_IC_H - 1);
+    LCD.drawRect(BATT_X + BATT_W - 1, SB_IC_Y + 2, BATT_X + BATT_W, SB_IC_Y + SB_IC_H - 2);
+  }
+
+  this.demo = function (cease) {
+    var evt = { rssi : null, vol : null, batt : null }, rssi = 0, vol = 0, batt = 0;
+    if (cease) {
+      Object.keys(evt).forEach((e) => { if (evt[e] !== null) clearInterval(evt[e]); });
+    } else {
+      this.draw_static();
+      var self = this;
+      evt.rssi = setInterval(() => { self.update_rssi(rssi); rssi = (rssi + .1) % 1.1; }, 300);
+      evt.vol = setInterval(() => { self.update_volume(vol); vol = (vol + .1) % 1.1; }, 250);
+      evt.batt = setInterval(() => { self.update_battery(batt); batt = (batt + .1) % 1.1; }, 200);
+    }
+  }
+
+}
+
+md380.prototype.ReadSecurity = function (addr) {
+  var ret;
   D7.reset();
   SPI1.setup({sck:B3, miso:B4, mosi:B5, baud:84000000, mode:3});
   SPI1.send([0x48, (addr>>16)&0xff, (addr>>8)&0xff, (addr)&0xff, 0xA5]);
@@ -12,6 +126,14 @@ md380.prototype.ReadSecurity = function(addr) {
   return ret;
 };
 
-exports.get = function() {
+// const COLOR_BG = [ .45, .63, .90 ]; // Background
+// LCD.setColor.apply(LCD, COLOR_BG);
+// LCD.fillRect(0, 0, 159, 114);
+// var md = require('md380').get();
+// var sb = new md.StatusBar();
+// sb.draw_static();
+// sb.demo();
+
+exports.get = function () {
 	return new md380();
 }
